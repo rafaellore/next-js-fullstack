c7c168cb523e652798c9184eb1702159d4bc3719
rafaellore <birdneon3@gmail.com>
2024-10-23 13:13:27 -0300

    feat(prisma): adds db integration and consume posts on home

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,33 @@
-import { CardPost } from "@/components/CardPost"
-import logger from "@/logger"
+import { CardPost } from "@/components/CardPost";
+import logger from "@/logger";
 
-import styles from './page.module.css'
-import Link from "next/link"
+import styles from "./page.module.css";
+import Link from "next/link";
 
-async function getAllPosts (page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
-  if (!response.ok) {
-    logger.error('Ops, alguma coisa correu mal')
-    return []
+async function getAllPosts(page) {
+  try {
+    const posts = await db.post.findMany();
+
+    return { data: posts, prev: null, next: null };
+  } catch (error) {
+    logger.error("Falha ao obter posts", { error });
+
+    return { data: [], prev: null, next: null };
   }
-  logger.info('Posts obtidos com sucesso')
-  return response.json()
 }
 
 export default async function Home({ searchParams }) {
-  const currentPage = searchParams?.page || 1
-  const { data: posts, prev, next } = await getAllPosts(currentPage)
+  const currentPage = searchParams?.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
   return (
     <main className={styles.grid}>
-      {posts.map(post =>  <CardPost key={post.id} post={post} />)}
+      {posts.map((post) => (
+        <CardPost key={post.id} post={post} />
+      ))}
       <div className={styles.links}>
         {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
         {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
       </div>
     </main>
-  )
+  );
 }
