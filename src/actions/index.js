ef4a6f7dd0b19d5e81d9e069a3c75a79d27c0853
rafaellore <birdneon3@gmail.com>
2024-10-24 15:04:10 -0300

    feat(prisma): adds comments model

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+"use server";
+
+import db from "../../prisma/db";
+
+export async function incrementThumbsUp(post) {
+  await db.post.update({
+    where: {
+      id: post.id,
+    },
+    data: {
+      likes: {
+        increment: 1,
+      },
+    },
+  });
+}
