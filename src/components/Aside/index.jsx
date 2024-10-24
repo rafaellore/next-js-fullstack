e1f925018edec40bb2135adc8e1b20fdad514d41
rafaellore <birdneon3@gmail.com>
2024-10-24 15:04:43 -0300

    feat(components): prepares aside and cardpost to comments

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,11 +1,14 @@
-import Image from 'next/image'
-import styles from './aside.module.css'
-
-import logo from './logo.png'
+import Image from "next/image";
+import styles from "./aside.module.css";
+import logo from "./logo.png";
+import Link from "next/link";
 
 export const Aside = () => {
-    return (<aside className={styles.aside}>
-        {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-        <Image src={logo} alt="Logo da Code Connect"/>
-    </aside>)
-}
+  return (
+    <aside className={styles.aside}>
+      <Link href="/">
+        <Image src={logo} alt="Logo da Code Connect" />
+      </Link>
+    </aside>
+  );
+};
